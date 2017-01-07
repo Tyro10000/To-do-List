f82dc12137870bf7e8e1007d895e3fd06c6699df
Tyro10000 <tmcelrat@kent.edu>
2017-01-07 10:08:54 -0500

    simple checklist

---
+++ list.js
@@ -0,0 +1,51 @@
+var Items = [];
+
+function add(){
+    Items.push(document.getElementById("newitem").value);
+    localStorage.Items = JSON.stringify(Items);
+    document.getElementById("newitem").value = "";
+    refresh();
+}
+function refresh(){
+    Items = JSON.parse(localStorage.Items);
+    relist();
+}
+
+function relist(){
+    document.getElementById("content-frame").innerHTML =  "";
+    for(x = 0; x < Items.length; x++){
+        var dus = '"' + Items[x] + '"';
+        document.getElementById("content-frame").innerHTML +="<div  class='frameitem' id="+Items[x]+">" +Items[x] + "<i class='fa fa-times pull-right' onclick='pop("+dus+")'></i></div>";
+    }
+}
+function clean(){
+    delete localStorage.Items;
+    Items = [];
+    document.getElementById("content-frame").innerHTML =  "";
+
+}
+
+refresh();
+
+function flag(line){
+ if (document.getElementById(line).style.color == "red")
+        document.getElementById(line).style.color  = "blue";
+else if (document.getElementById(line).style.color == "blue")
+        document.getElementById(line).style.color  = "white";
+else 
+        document.getElementById(line).style.color  = "red";
+
+}
+function pop(line){
+    removeByValue(line);
+    refresh();
+}
+function removeByValue(val) {
+	for(var i=0; i<Items.length; i++) {
+		if(Items[i] == val) {
+			Items.splice(i, 1);
+            localStorage.Items = JSON.stringify(Items);
+			break;
+		}
+	}
+}
