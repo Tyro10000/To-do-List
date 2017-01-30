591ecffb57cdc197e6b6f7aab4533eca69fcc591
tyrone mcelrath <tyrone mcelrath>
2017-01-30 17:51:53 -0500

    change style

--- list.js
+++ list.js
@@ -1,51 +1,55 @@
 var Items = [];
 
-function add(){
+function add() {
     Items.push(document.getElementById("newitem").value);
     localStorage.Items = JSON.stringify(Items);
     document.getElementById("newitem").value = "";
     refresh();
 }
-function refresh(){
+
+function refresh() {
     Items = JSON.parse(localStorage.Items);
     relist();
 }
 
-function relist(){
-    document.getElementById("content-frame").innerHTML =  "";
-    for(x = 0; x < Items.length; x++){
+function relist() {
+    document.getElementById("content-frame").innerHTML = "";
+    for (x = 0; x < Items.length; x++) {
         var dus = '"' + Items[x] + '"';
-        document.getElementById("content-frame").innerHTML +="<div  class='frameitem' id="+dus+">" +Items[x] + "<i class='fa fa-check pull-right' onclick='flag("+dus+")'></i><i class='fa fa-times pull-right' onclick='pop("+dus+")'></i></div>";
+        document.getElementById("content-frame").innerHTML += "<div  class='frameitem' id=" + dus + ">" + Items[x] + "<i class='fa fa-check pull-right' onclick='flag(" + dus + ")'></i><i class='fa fa-times pull-right' onclick='pop(" + dus + ")'></i></div>";
     }
 }
-function clean(){
+
+function clean() {
     delete localStorage.Items;
     Items = [];
-    document.getElementById("content-frame").innerHTML =  "";
+    document.getElementById("content-frame").innerHTML = "";
 
 }
 
 refresh();
 
-function flag(line){
- if (document.getElementById(line).style.color == "red")
-        document.getElementById(line).style.color  = "lightgreen";
-else if (document.getElementById(line).style.color == "lightgreen")
-        document.getElementById(line).style.color  = "white";
-else 
-        document.getElementById(line).style.color  = "red";
+function flag(line) {
+    if (document.getElementById(line).style.color == "red")
+        document.getElementById(line).style.color = "lightgreen";
+    else if (document.getElementById(line).style.color == "lightgreen")
+        document.getElementById(line).style.color = "black";
+    else
+        document.getElementById(line).style.color = "red";
 
 }
-function pop(line){
+
+function pop(line) {
     removeByValue(line);
     refresh();
 }
+
 function removeByValue(val) {
-	for(var i=0; i<Items.length; i++) {
-		if(Items[i] == val) {
-			Items.splice(i, 1);
+    for (var i = 0; i < Items.length; i++) {
+        if (Items[i] == val) {
+            Items.splice(i, 1);
             localStorage.Items = JSON.stringify(Items);
-			break;
-		}
-	}
+            break;
+        }
+    }
 }
